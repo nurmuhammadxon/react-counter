2fd78144c7ca7c8dcb946dbc6f6df749b68ebcb7
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2025-08-10 16:51:07 +0500

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// React-Redux
+import { Provider } from "react-redux";
+import store from "../store/store.js";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
+);
